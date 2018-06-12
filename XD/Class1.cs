4b9fbdd79d6967c41ac7960d21ca97d73845a696
Jakob Vestergaard <jakobmetalv@gmail.com>
2018-06-12 16:31:31 +0200

    Du gay

---
+++ XD/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XD
+{
+    class Class1
+    {
+        static void DuErGay()
+        {
+            Console.WriteLine("Du er gay");
+        }
+    }
+}
